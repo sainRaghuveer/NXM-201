4411b1108e5d0d0f2c35b87c191b8d09eb2e9389
sainRaghuveer <raghuveersain987@gmail.com>
2023-07-07 23:13:20 +0530

    update

---
+++ day-6/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const winston = require('winston');
+const expressWinston = require('express-winston');
+// require("winston-mongodb");
+
+
+const app = express();
+
+app.use(expressWinston.logger({
+    transports:[
+        new winston.transports.Console({
+            json: true,
+            colorize: true,
+            level:"info",
+        }),
+        new winston.transports.File({
+            level:"info",
+            filename:"infologs.log"
+        })
+    ],
+    format:winston.format.prettyPrint()
+}));
+
+app.get("/", (req, res) => {
+    res.send("Home page...!");
+});
+
+app.get("/results", (req, res) => {
+    res.send("Results");
+});
+
+app.get("/reports", (req, res) => {
+    const {token} = req.query.token;
+    if(token!==123){
+        res.send("Invalid token");
+    }else{
+        res.send("Home page...!");
+    }
+});
+
+app.listen(8000, ()=>{
+    console.log("Running the server at port 8000...");
+});
+
